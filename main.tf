bdf85928365b88a6d0048de02b66405f2a6925a8
Tomas Aguilera <t.aguilera@uniandes.edu.co>
2025-10-16 12:47:59 -0500

    Update subnet data source to use filter for VPC ID

--- main.tf
+++ main.tf
@@ -101,9 +101,13 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
+
 
 ##############################
 # EC2: BASE DE DATOS (PostgreSQL)
